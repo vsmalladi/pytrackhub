3b98012b0061ec6aef95d13a415021d9ce3fcd17
Venkat Malladi <vsmalladi@gmail.com>
2012-06-24 13:37:53 -0700

    Added basic variable assigning for track object.

--- trackhub/track.py
+++ trackhub/track.py
@@ -1,9 +1,9 @@
 class Track(object):
     """ Base class for a track object. """
 
-    def __init__(self):
-        self.track_name = None
-        self.big_data_url = None
-        self.short_label = None
-        self.long_label = None
-        self.track_type = None
+    def __init__(self,track_name=None,big_data_url=None,short_label=None,long_label=None,track_type=None):
+        self.track_name = track_name
+        self.big_data_url = big_data_url
+        self.short_label = short_label
+        self.long_label = long_label
+        self.track_type = track_type
