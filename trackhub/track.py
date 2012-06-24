7295fd247312080f849b65f600a53faf69c26b9e
Venkat Malladi <vsmalladi@gmail.com>
2012-06-24 13:26:13 -0700

    Basic layout of track object, 5 minimal values needed for a track.

---
+++ trackhub/track.py
@@ -0,0 +1,9 @@
+class Track(object):
+    """ Base class for a track object. """
+
+    def __init__(self.name):
+        self.track_name = None
+        self.big_data_url = None
+        self.short_label = None
+        self.long_label = None
+        self.track_type = None
