a56421f30bcb2962405b5dace23473bfa1a7bb4d
Venkat Malladi <vmalladi@stanford.edu>
2012-07-19 12:35:59 -0700

    Updated some spacing and changed the order method to be private.

--- trackhub/track.py
+++ trackhub/track.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 class Track(object):
     """ Base class for a track object. """
 
-    def __init__(self,track_name=None,big_data_url=None,short_label=None,long_label=None,track_type=None):
+    def __init__(self, track_name=None, big_data_url=None, short_label=None, long_label=None, track_type=None):
         self.track_name = track_name
         self.big_data_url = big_data_url
         self.short_label = short_label
@@ -11,7 +11,7 @@ class Track(object):
         self.track_type = track_type
         self.ordered_attributes = OrderedDict()
 
-    def order(self):
+    def __order(self):
         """Order 5 basic attributes that should be set to match style of trackHubs"""
         self.ordered_attributes['trackName'] = self.track_name
         self.ordered_attributes['bigDataUrl'] = self.big_data_url
@@ -21,7 +21,7 @@ class Track(object):
 
     def __str__(self):
         str = ''
-        self.order()
+        self.__order()
         for var,val in self.ordered_attributes.items():
             str += var + ' ' + val + '\n'
 
