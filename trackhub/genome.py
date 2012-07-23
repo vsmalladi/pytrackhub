f1e411dd609853493a860d4e8432c9a012f23e2f
Venkat Malladi <vmalladi@stanford.edu>
2012-07-23 11:00:38 -0700

    Fixed reference names and typo's, items missed in commit 28092ee02c473881a3ea6e68b1e52ee84e848027.

--- trackhub/genome.py
+++ trackhub/genome.py
@@ -22,29 +22,31 @@ class GenomeStanza(object):
         """ Adds another track object into the list of tracks for a given genome"""
         self.track_list.append(track_object)
 
-    def write_track(self, track_object):
+    def write_track(self, track_object, track_dir):
         """Write out each track stanza in its own file"""
-        track_object_path = os.path.join(self.genome,track_object.track_name)
+        track_object_path = os.path.join(track_dir,track_object.track_name)
         track_object_file = open(track_object_path + '.txt','w')
         track_object.write(track_object_file)
         track_object_file.close()
 
-    def write_track_file(self):
+    def write_track_file(self,genome_dir):
         """Write the include file and loop through and write each tack object """
-        if not os.path.exists(self.genome):
-            os.mkdir(self.genome)
+        genome_object_path = os.path.join(genome_dir,self.genome)
         
-        track_file_path = os.path.join(self.genome,self.track_file)
+        if not os.path.exists(genome_object_path):
+            os.mkdir(genome_object_path)
+        
+        track_file_path = os.path.join(genome_object_path,self.track_file)
         track_file = open(track_file_path,'w')
         
         for track in self.track_list:
             track_file.write('include ' + track.track_name + '.txt' + '\n')
-            self.write_track(track)
+            self.write_track(track,genome_object_path)
 
         track_file.close()
 
     def write(self, fh):
-        """Write genome listi into a file"""
+        """Write genome list into a file"""
         fh.write(str(self))
         fh.write('\n')
     
@@ -52,7 +54,7 @@ class GenomeStanza(object):
 class GenomeFile(object):
     """" Class for holding all the genome stanza's. """
 
-    def __init__(self,genome_file=None):
+    def __init__(self,genome_file='genomes.txt'):
         self.genome_list = []
         self.genome_file = genome_file
 
@@ -67,16 +69,19 @@ class GenomeFile(object):
         """Adds another genome object into the list of genomes for a given trackHub """
         self.genome_list.append(genome_object)
 
-    def write(self):
+    def write(self,hub_dir):
         """Write list of genomes out to a genome file."""
-        genome_file = open(self.genome_file,'w')
+        genome_file_path = os.path.join(hub_dir,self.genome_file)
+        genome_file = open(genome_file_path,'w')
+
         genome_file.write(str(self))
         genome_file.close()
 
-    def write_genome_dir(self):
+    def write_genome_dir(self,hub_path):
         """Write the files associated for a track in appropiate directory"""
+        self.write(hub_path)
         for genome in self.genome_list:
-            genome.write_track_file()
+            genome.write_track_file(hub_path)
     
 
     def add_track(self,genome_object,track_object):
